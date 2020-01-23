a7ffc4f3095648289f422fafe6c1863181399012
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-23 17:49:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,10 +8,11 @@ def directors_totals(nds)
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   nds.each do |elem|
-    puts elem
     puts elem[:name]
     puts "blank"
-    puts elem[:movies]
+    puts elem[:movies].reduce(0) do |sum, element|
+      sum + element[:worldwide_gross]
+    end
   end
   result = {
   }
